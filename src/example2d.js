7e5c858091bc38e6ba3a80580964beb19c4d30e8
Ekaterina Semchuk <cat_as@mail.ru>
2025-08-21 00:37:23 +0300

    basics for 3D parser

---
+++ src/example2d.js
@@ -0,0 +1,53 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { DXFLibLoader } from "./loaders/dxfLib2D.js";
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+let canvas = document.getElementById("viewer");
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#A9A9A9");
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+camera.position.z = 5;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0);
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+
+const loader = new DXFLibLoader();
+loader.load(
+  "/public/models/model.dxf",
+  (model) => {
+    console.log("loaded");
+    model.entities.forEach((entity) => {
+      scene.add(entity);
+    });
+    renderer.render(scene, camera);
+  },
+  () => {
+    console.log("loading...");
+  },
+  (e) => {
+    console.log(e);
+  }
+);
+
+animate();
